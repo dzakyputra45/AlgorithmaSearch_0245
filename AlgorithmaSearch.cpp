f7ed0215d8378a2ac7a42f8651ff825637aa355a
dzakyputra45 <dzakypratama45@gmail.com>
2024-04-04 15:22:56 +0700

    Perulangan perbandingan jika arr sama dengan item

--- AlgorithmaSearch.cpp
+++ AlgorithmaSearch.cpp
@@ -42,6 +42,11 @@ void linearSearch()
 
         ctr = 0;
         i = 0; //step 2
+        while (i < n) //step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+        }
     }
     
 }
